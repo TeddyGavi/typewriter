53d12966866b62e828231fa05d932ec11fc9d73c
Matthew Davis <matc.davis@gmail.com>
2022-08-03 21:20:00 +0000

    created a function using timeouts to print to the console each letter of a given string like a typewriter

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,21 @@
-typewriter
+// typewriter
+//prints to the terminal each character from a given sentence, afterwards a newline is added 
+const sentence = "hello there from lighthouse labs";
+
+const typewriter = (sentence) => {
+  let x = 0;
+  while (x < sentence.length) {
+    for (const i of sentence) {
+      setTimeout(() => {
+        process.stdout.write(i);
+
+      }, x);
+      x += 50;
+    }
+    setTimeout(() => {
+      process.stdout.write("\n");
+    }, x);
+  }
+};
+
+typewriter(sentence);
